a605629a8ce4fad68af9c96a1300c5fcea02d569
798141652 <33684467+798141652@users.noreply.github.com>
2019-11-21 23:34:08 +0800

    Add files via upload

---
+++ 189.java
@@ -0,0 +1,21 @@
+//给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
